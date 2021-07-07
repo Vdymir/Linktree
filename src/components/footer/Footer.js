9822dc0d27bc0c586c3071be606a4368872c7407
mrLuisFer <mrluisfeer@gmail.com>
2021-07-07 18:49:26 -0500

    formating code before adding some rules

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Creation } from './footer-styled'
+import React from "react";
+import { Creation } from "./footer-styled";
 
 const Footer = () => {
-    return (
-        <Creation>
-            <p>LinkTree by: <span>Vladimir</span></p>
-        </Creation>
-    )
-}
+  return (
+    <Creation>
+      <p>
+        LinkTree by: <span>Vladimir</span>
+      </p>
+    </Creation>
+  );
+};
 
-export default Footer
+export default Footer;
